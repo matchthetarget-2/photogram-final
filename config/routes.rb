6f03dde42d22f5f6514b0db4b1916a19a616df1e
Ben Purinton <ben.purinton@gmail.com>
2023-05-24 09:11:17 -0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,126 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Routes for the Like resource:
+
+  # CREATE
+  post("/insert_like", { :controller => "likes", :action => "create" })
+          
+  # READ
+  get("/likes", { :controller => "likes", :action => "index" })
+  
+  get("/likes/:path_id", { :controller => "likes", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_like/:path_id", { :controller => "likes", :action => "update" })
+  
+  # DELETE
+  get("/delete_like/:path_id", { :controller => "likes", :action => "destroy" })
+
+  #------------------------------
+
+  # Routes for the Follow request resource:
+
+  # CREATE
+  post("/insert_follow_request", { :controller => "follow_requests", :action => "create" })
+          
+  # READ
+  get("/follow_requests", { :controller => "follow_requests", :action => "index" })
+  
+  get("/follow_requests/:path_id", { :controller => "follow_requests", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_follow_request/:path_id", { :controller => "follow_requests", :action => "update" })
+  
+  # DELETE
+  get("/delete_follow_request/:path_id", { :controller => "follow_requests", :action => "destroy" })
+
+  #------------------------------
+
+  # Routes for the Comment resource:
+
+  # CREATE
+  post("/insert_comment", { :controller => "comments", :action => "create" })
+          
+  # READ
+  get("/comments", { :controller => "comments", :action => "index" })
+  
+  get("/comments/:path_id", { :controller => "comments", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_comment/:path_id", { :controller => "comments", :action => "update" })
+  
+  # DELETE
+  get("/delete_comment/:path_id", { :controller => "comments", :action => "destroy" })
+
+  #------------------------------
+
+  # Routes for the User account:
+
+  # SIGN UP FORM
+  get("/user_sign_up", { :controller => "user_authentication", :action => "sign_up_form" })        
+  # CREATE RECORD
+  post("/insert_user", { :controller => "user_authentication", :action => "create"  })
+      
+  # EDIT PROFILE FORM        
+  get("/edit_user_profile", { :controller => "user_authentication", :action => "edit_profile_form" })       
+  # UPDATE RECORD
+  post("/modify_user", { :controller => "user_authentication", :action => "update_private" })
+  
+  # DELETE RECORD
+  get("/cancel_user_account", { :controller => "user_authentication", :action => "destroy" })
+
+ # READ
+ get("/", { :controller => "user_authentication", :action => "index" })
+
+ get("/users", { :controller => "user_authentication", :action => "index" })
+  
+ get("/users/:the_username", { :controller => "user_authentication", :action => "show" })
+
+ get("/not_following", { :controller => "user_authentication", :action => "not_following" })
+
+ get("/users/:the_username/liked_photos", { :controller => "user_authentication", :action => "liked_photos_show" })
+
+ get("/users/:the_username/feed", { :controller => "user_authentication", :action => "feed_show" })
+
+  # ------------------------------
+
+  # SIGN IN FORM
+  get("/user_sign_in", { :controller => "user_authentication", :action => "sign_in_form" })
+
+  get("/user_sign_in_error", { :controller => "user_authentication", :action => "sign_in_form_error" })
+
+  get("/user_sign_in_error_photo", { :controller => "user_authentication", :action => "sign_in_form_error_photo" })
+
+  get("/user_not_following", { :controller => "user_authentication", :action => "not_folloing_error" })
+
+  # AUTHENTICATE AND STORE COOKIE
+  post("/user_verify_credentials", { :controller => "user_authentication", :action => "create_cookie" })
+  
+  # SIGN OUT        
+  get("/user_sign_out", { :controller => "user_authentication", :action => "destroy_cookies" })
+             
+  #------------------------------
+
+  # Routes for the Photo resource:
+
+  # CREATE
+  post("/insert_photo", { :controller => "photos", :action => "create" })
+          
+  # READ
+  get("/photos", { :controller => "photos", :action => "index" })
+  
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_photo/:path_id", { :controller => "photos", :action => "update" })
+  
+  # DELETE
+  get("/delete_photo/:path_id", { :controller => "photos", :action => "destroy" })
+
+  #------------------------------
+
 end
