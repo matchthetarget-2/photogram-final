95bea7e513566b429d695538e9002896a9554c22
Ben Purinton <ben.purinton@gmail.com>
2023-05-23 17:14:17 -0800

    config/application

--- config/application.rb
+++ config/application.rb
@@ -6,17 +6,30 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module VanillaRails7
+module PhotogramFinal
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 6.0
+    config.generators do |g|
+      g.test_framework nil
+      g.factory_bot false
+      g.scaffold_stylesheet false
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+    end
+    # Load AdminUser model
+    config.autoload_paths += %W(#{config.root}/vendor/app/models)
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.action_controller.default_protect_from_forgery = false
+    config.active_record.belongs_to_required_by_default = false
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
   end
 end
