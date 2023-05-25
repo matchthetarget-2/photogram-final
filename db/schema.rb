30fd678d330665c01482ec8162226faf210b9913
Ben Purinton <ben.purinton@gmail.com>
2023-05-24 21:17:04 -0400

    rebuild app (#3)
    * copied specs from appdev-projects
    
    * added migrations
    
    * update database.yml
    
    * bin/setup
    
    * models
    
    * controllers
    
    * views
    
    * routes
    
    * annotate models with bin/setup
    
    * change keyword args for rails 7
    
    * remove .html.erb extensions
    
    * spec support images
    
    * no fetch on session
    
    * sample_data
    
    * carrierwave and cloudinary gems
    
    * ImageUploader
    
    * carrierwave and cloudinary initializers
    
    * change keyword args for rails 7 again
    
    * turn on appdev_support
    
    * add cloudinary creds
    
    * seems to work minus a few specs
    
    * fixed image upload on form
    
    * stuck on image_identifier
    
    * added carrierwave to spec_helper
    
    * specs passing

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_16_013012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "author_id"
+    t.text "body"
+    t.integer "photo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "follow_requests", force: :cascade do |t|
+    t.integer "recipient_id"
+    t.integer "sender_id"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "fan_id"
+    t.integer "photo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.text "caption"
+    t.integer "comments_count"
+    t.string "image"
+    t.integer "likes_count"
+    t.integer "owner_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.integer "comments_count"
+    t.integer "likes_count"
+    t.boolean "private"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
